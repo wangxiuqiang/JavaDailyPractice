6cd3512d5f5204a1673ea8283b1d1ff7ad98c3a0
wangxiuqiang <wangxiuqianga@163.com>
2017-06-29 22:14:01 +0800

    2017夏实训

--- Hotel_Management/src/Dao/startRoom.java
+++ Hotel_Management/src/Dao/startRoom.java
@@ -1,22 +1,27 @@
 package Dao;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JMenu;
-import javax.swing.JCheckBox;
-import javax.swing.JTextField;
-import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.sql.ResultSet;
 
-public class startRoom implements ActionListener,ItemListener{
+import javax.imageio.event.IIOReadWarningListener;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JTextField;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
+import Jdbc.jdbcMysql;
+
+public class startRoom implements ActionListener, ItemListener {
 
 	private JFrame frame;
 	private JTextField food11;
@@ -27,164 +32,255 @@ public class startRoom implements ActionListener,ItemListener{
 	private JTextField food66;
 	private JTextField food77;
 	private JTextField food88;
-	JCheckBox food1,food2,food3,food4,food5,food6,food7,food8;
-	JButton queRenButton,backButton;
+	JCheckBox food1, food2, food3, food4, food5, food6, food7, food8;
+	JButton queRenButton, backButton;
+
 	public startRoom() {
 		initialize();
 		frame.setVisible(true);
 	}
 
-	
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel label = new JLabel("\u8BF7\u9009\u62E9\u60A8\u7684\u4F73\u80B4");
 		label.setFont(new Font("楷体", Font.BOLD, 18));
 		label.setBounds(141, 26, 157, 34);
 		frame.getContentPane().add(label);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 105, 21);
 		frame.getContentPane().add(menuBar);
-		
+
 		JMenu food = new JMenu("菜品");
 		menuBar.add(food);
-		
+
 		JMenu soup = new JMenu("汤类");
 		menuBar.add(soup);
-		
+
 		JMenu driver = new JMenu("酒品");
 		menuBar.add(driver);
-		
-		 food1 = new JCheckBox("1");
+
+		food1 = new JCheckBox("1");
 		food1.setBounds(24, 66, 94, 34);
 		frame.getContentPane().add(food1);
-		
+
 		food11 = new JTextField();
 		food11.setBounds(136, 73, 39, 21);
 		frame.getContentPane().add(food11);
 		food11.setColumns(10);
-		
-		 food2 = new JCheckBox("2");
+
+		food2 = new JCheckBox("2");
 		food2.setBounds(24, 102, 94, 34);
 		frame.getContentPane().add(food2);
-		
-		 food3 = new JCheckBox("3");
+
+		food3 = new JCheckBox("3");
 		food3.setBounds(24, 138, 94, 34);
 		frame.getContentPane().add(food3);
-		
-		 food4 = new JCheckBox("4");
+
+		food4 = new JCheckBox("4");
 		food4.setBounds(24, 174, 94, 34);
 		frame.getContentPane().add(food4);
-		
+
 		food22 = new JTextField();
 		food22.setBounds(136, 109, 39, 21);
 		frame.getContentPane().add(food22);
 		food22.setColumns(10);
-		
+
 		food33 = new JTextField();
 		food33.setColumns(10);
 		food33.setBounds(136, 145, 39, 21);
 		frame.getContentPane().add(food33);
-		
+
 		food44 = new JTextField();
 		food44.setColumns(10);
 		food44.setBounds(136, 181, 39, 21);
 		frame.getContentPane().add(food44);
-		
-		 food5 = new JCheckBox("5");
+
+		food5 = new JCheckBox("5");
 		food5.setBounds(237, 66, 94, 34);
 		frame.getContentPane().add(food5);
-		
-		 food6 = new JCheckBox("6");
+
+		food6 = new JCheckBox("6");
 		food6.setBounds(237, 102, 94, 34);
 		frame.getContentPane().add(food6);
-		
-		 food7 = new JCheckBox("7");
+
+		food7 = new JCheckBox("7");
 		food7.setBounds(237, 138, 94, 34);
 		frame.getContentPane().add(food7);
-		
-		 food8 = new JCheckBox("8");
+
+		food8 = new JCheckBox("8");
 		food8.setBounds(237, 174, 94, 34);
 		frame.getContentPane().add(food8);
-		
+
 		food55 = new JTextField();
 		food55.setColumns(10);
 		food55.setBounds(332, 73, 39, 21);
 		frame.getContentPane().add(food55);
-		
+
 		food66 = new JTextField();
 		food66.setColumns(10);
 		food66.setBounds(332, 109, 39, 21);
 		frame.getContentPane().add(food66);
-		
+
 		food77 = new JTextField();
 		food77.setColumns(10);
 		food77.setBounds(332, 145, 39, 21);
 		frame.getContentPane().add(food77);
-		
+
 		food88 = new JTextField();
 		food88.setColumns(10);
 		food88.setBounds(332, 181, 39, 21);
 		frame.getContentPane().add(food88);
-		
-		 queRenButton = new JButton("确认");
+
+		queRenButton = new JButton("确认");
 		queRenButton.setBounds(115, 214, 71, 23);
 		frame.getContentPane().add(queRenButton);
-		
-		 backButton = new JButton("返回");
+
+		backButton = new JButton("返回");
 		backButton.setBounds(227, 214, 71, 23);
 		frame.getContentPane().add(backButton);
-		
+
 		JLabel label_1 = new JLabel("/\u4EFD");
 		label_1.setBounds(176, 76, 39, 15);
 		frame.getContentPane().add(label_1);
-		
+
 		JLabel label_2 = new JLabel("/\u4EFD");
 		label_2.setBounds(176, 112, 39, 15);
 		frame.getContentPane().add(label_2);
-		
+
 		JLabel label_3 = new JLabel("/\u4EFD");
 		label_3.setBounds(176, 148, 39, 15);
 		frame.getContentPane().add(label_3);
-		
+
 		JLabel label_4 = new JLabel("/\u4EFD");
 		label_4.setBounds(176, 184, 39, 15);
 		frame.getContentPane().add(label_4);
-		
+
 		JLabel label_5 = new JLabel("/\u4EFD");
 		label_5.setBounds(372, 76, 39, 15);
 		frame.getContentPane().add(label_5);
-		
+
 		JLabel label_6 = new JLabel("/\u4EFD");
 		label_6.setBounds(372, 112, 39, 15);
 		frame.getContentPane().add(label_6);
-		
+
 		JLabel label_7 = new JLabel("/\u4EFD");
 		label_7.setBounds(372, 148, 39, 15);
 		frame.getContentPane().add(label_7);
-		
+
 		JLabel label_8 = new JLabel("/\u4EFD");
 		label_8.setBounds(372, 184, 39, 15);
 		frame.getContentPane().add(label_8);
 	}
 
+	jdbcMysql jd = new jdbcMysql();
 
-	
-	public void itemStateChanged(ItemEvent e) {
-	   if(e.getItem() == food1){
-		   
-	   }
+	public void jdbcDao(int id) throws Exception {
+		Connection conn = (Connection) jd.getConn();
+		Statement statement = (Statement) conn.createStatement();
+		String sql = "select name,price from menu where id = " + id;
+		ResultSet rs = statement.executeQuery(sql);
+		String name = null;
+		int price = 0;
+		while (rs.next()) {
+			name = rs.getString("name");
+			price = rs.getInt("price");
+		}
+		String sql2 = "insert into menuIn(id,name,price) values(" + "" + " '" + name + "' " + price;
+		statement.execute(sql2);
+		statement.close();
+		rs.close();
 	}
 
+	public void itemStateChanged(ItemEvent e) {
+		if (e.getItem() == food1) {
+			try {
+				jdbcDao(1);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}
+		if (e.getItem() == food1) {
+			if (food2.isSelected() == true) {
+				try {
+					jdbcDao(1);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		if (e.getItem() == food2) {
+			if (food2.isSelected() == true) {
+				try {
+					jdbcDao(2);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		if (e.getItem() == food3) {
+			if (food2.isSelected() == true) {
+				try {
+					jdbcDao(3);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		if (e.getItem() == food4) {
+			if (food2.isSelected() == true) {
+				try {
+					jdbcDao(4);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		if (e.getItem() == food5) {
+			if (food2.isSelected() == true) {
+				try {
+					jdbcDao(5);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		if (e.getItem() == food6) {
+			if (food2.isSelected() == true) {
+				try {
+					jdbcDao(6);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
 
-	
+		}
+		if (e.getItem() == food7) {
+			if (food2.isSelected() == true) {
+				try {
+					jdbcDao(7);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		if (e.getItem() == food8) {
+			if (food2.isSelected() == true) {
+				try {
+					jdbcDao(8);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+
+	}
+
 	public void actionPerformed(ActionEvent e) {
-	
+
 	}
 }
-
-
